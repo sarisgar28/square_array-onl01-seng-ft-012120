e7d8c01cb286bff8833265eecceb0aa863574414
Sara Isabel Garcia Reyes <sarisgar28@gmail.com>
2020-01-27 15:48:22 +0000

    Automatically backed up by Learn

--- square_array.rb
+++ square_array.rb
@@ -1,4 +1,4 @@
-primary_colors = ["red, yellow, blue"]
+ def primary_colors = ["red, yellow, blue"]
   #primary_colors.each do|color|
   puts "primary color is #{color}is#{color.length}letters long"
   end
