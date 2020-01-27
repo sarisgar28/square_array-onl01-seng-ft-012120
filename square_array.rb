fb62b57a1dc35b45e1b689ae5fe5bee7076b71a9
Sara Isabel Garcia Reyes <sarisgar28@gmail.com>
2020-01-27 15:53:04 +0000

    Done.

--- square_array.rb
+++ square_array.rb
@@ -1,3 +1,6 @@
-def square_array(array)
-  # your code here
-end
+ def primary_colors = ["red, yellow, blue"]
+  #primary_colors.each do|color|
+  puts "primary color is #{color}is#{color.length}letters long"
+  end
+  #/nil
+  
